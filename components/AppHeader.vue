d310e9ef275afb045f9f7b7b2c3462e33a9d86da
Codaea <dakotajayroth@gmail.com>
2024-03-29 09:20:58 -0700

    Add Pages and A nav Header and Footer to each page *Refactor app.vue to use NuxtLayout and NuxtPage components.

---
+++ components/AppHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Header With Nav Bar!</h1>
+    <div class="links">
+      <nuxt-link to="/">Home</nuxt-link>
+      <nuxt-link to="/about">About</nuxt-link>
+      <nuxt-link to="/join">Join</nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+  
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Landing Page</h1>
+    <span>Logo</span>
+    <h1>Hero Card</h1>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
