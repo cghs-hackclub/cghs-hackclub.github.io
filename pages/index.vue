d3cd45c434260dccaef31c33702426d0dece3030
Codaea <dakotajayroth@gmail.com>
2024-04-06 10:35:26 -0700

    Refactor index.vue to update Hack Club content and add new sections *

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,44 @@
 <template>
-  <div class="flex items-center justify-center h-screen bg-gray-900"> <!-- put something cool here? -->
-    <img src="~/public/assets/hack-cghs-logo-dark.svg" alt="Hack Club Logo">
-  </div>
-  <div>
-    <p>more content about hack club before the footer.</p>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates pariatur fugiat assumenda consequatur ipsam itaque aliquam doloremque enim tempore delectus obcaecati veniam ut saepe doloribus, dolorem harum. Architecto, quos.</p>
+  <div class="flex flex-col">
+    <div class="flex items-center justify-center h-screen bg-gray-900 shadow-2xl">
+      <!-- put something cool here? READ MY SLACK NOTES
+      run the fractal Jam backgrounds we make aas the background of the logo 
+      from shadertoy.com
+      
+      -->
+      <img src="~/public/assets/hack-cghs-logo-dark.svg" alt="Hack Club Logo" />
+      <br />
+      <h1 class="text-white">scroll down mark</h1>
+    </div>
+    <div
+      class="flex flex-row h-[40rem] items-center justify-center"
+    >
+      <h1 class="ml-8 text-7xl">
+        We Aren't "Hacking The Mainframe."
+      </h1>
+      <div>
+        <p class=" text-3xl">
+          Hack Club is a space for people who like to make and understand how
+          things work. we do [workshops and jams](), where we make our own
+          software and hardware.
+        </p>
+
+        <p class="mt-5 text-m">
+          check out the <a class="underline text-blue-500" href="https://hackclub.com/philosophy/">Hacker Ethic</a> for more.
+        </p>
+      </div>
+    </div>
+    <div class="flex flex-col h-96 space-y-8 items-center justify-center">
+      <h1 class="ml-8 text-7xl w-[40rem]">Things Our Hackers Have Made!</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa perspiciatis eveniet, dolorem eos impedit quos repudiandae asperiores officia soluta eaque aspernatur voluptate iure fugit consequatur facere, nostrum consequuntur, reiciendis odio.</p>
+    </div>
+    <div class="h-96 ">
+      <p class="">Join Us</p>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import logo from '~/public/assets/hack-cghs-logo.svg'
 </script>
 
-<style>
-</style>
+<style></style>
