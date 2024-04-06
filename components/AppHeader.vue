dcf6802c0cf3f5c3bec5d143019e295f2485bd39
Codaea <dakotajayroth@gmail.com>
2024-04-05 17:20:38 -0700

    added orpheus to nav bar, linking to hackclub

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
-    <h1>Header With Nav Bar!</h1>
-    <div class="links">
+    <div class="h-20 flex space-x-8 items-center">
+      <nuxt-link to="https://hackclub.com" >
+        <img class="h-20 hover:animate-wiggle hover:animate-infinite" src="~/public/assets/flag-orpheus-top.svg">
+      </nuxt-link>
       <nuxt-link to="/">Home</nuxt-link>
       <nuxt-link to="/about">About</nuxt-link>
       <nuxt-link to="/join">Join</nuxt-link>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,10 @@
 <template>
+  <div class="flex items-center justify-center h-screen bg-slate-500"> <!-- put something cool here? -->
+    <h1 class="text-7xl ">Hack Club @ CGHS</h1>
+  </div>
   <div>
-    <h1>Landing Page</h1>
-    <span>Logo</span>
-    <h1>Hero Card</h1>
+    <p>more content about hack club before the footer.</p>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates pariatur fugiat assumenda consequatur ipsam itaque aliquam doloremque enim tempore delectus obcaecati veniam ut saepe doloribus, dolorem harum. Architecto, quos.</p>
   </div>
 </template>
 
