9fc21e77a148d82716ecb03d3eece3c5164dfd9b
Dakota Roth <93889672+Codaea@users.noreply.github.com>
2024-05-21 04:09:46 +0000

    converted image cards to template *added guides index page

---
+++ components/image-card.vue
@@ -0,0 +1,15 @@
+<script setup>
+    const props =  defineProps([ 'image', 'title', 'description', 'linkUrl' ])
+</script>
+
+<template>
+    <a :href=props.linkUrl> <!-- TODO: push these into templates in components-->
+        <div class="bg-gray-800 m-6 w-96 h-[18rem] rounded-2xl border-8 border-gray-800 shadow-2xl cursor-pointer 
+        transition ease-in-out delay-30 hover:-translate-y-2 hover:translate-x-2 ">
+            <img class="rounded-2xl" :src=props.image>
+            <div class="flex items-center justify-center">
+                <p class="mt-4 text-white text-lg">{{ props.title }}</p>
+            </div>
+        </div>
+    </a>
+</template>
---
+++ pages/guides/index.vue
@@ -0,0 +1,6 @@
+<script setup>
+import basicCard from '~/components/image-card.vue';
+</script>
+<template>
+    <basicCard title="Build a Static Website!" image="https://kinsta.com/wp-content/uploads/2021/10/html-site-example.png" description="" link-url="/guides/"/> <!-- TODO: temp image, find a permanant -->
+</template>
