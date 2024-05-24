3eb7d38c1242e891d4b92cf98961cb249f1f4828
Dakota Roth <93889672+Codaea@users.noreply.github.com>
2024-05-24 01:25:39 +0000

    update image-card components

--- components/image-card.vue
+++ components/image-card.vue
@@ -1,15 +1,21 @@
 <script setup>
-    const props =  defineProps([ 'image', 'title', 'description', 'linkUrl' ])
+const props = defineProps(['image', 'title', 'description', 'linkUrl'])
 </script>
 
 <template>
-    <a :href=props.linkUrl> <!-- TODO: push these into templates in components-->
-        <div class="bg-gray-800 m-6 w-96 h-[18rem] rounded-2xl border-8 border-gray-800 shadow-2xl cursor-pointer 
+    <nuxt-link :to=props.linkUrl> <!-- TODO: push these into templates in components-->
+        <div class="bg-gray-800 m-6 w-96 h-[24] rounded-2xl border-8 border-gray-800 shadow-2xl cursor-pointer 
         transition ease-in-out delay-30 hover:-translate-y-2 hover:translate-x-2 ">
-            <img class="rounded-2xl" :src=props.image>
-            <div class="flex items-center justify-center">
-                <p class="mt-4 text-white text-lg">{{ props.title }}</p>
+            <div class="flex justify-center">
+                <div class="flex flex-col">
+                    <div class="flex-1">
+                        <img class="rounded-2xl h-64" :src=props.image>
+                    </div>
+                    <div class="flex items-center justify-center">
+                        <p class="mt-4 text-white text-lg">{{ props.title }}</p>
+                    </div>
+                </div>
             </div>
         </div>
-    </a>
+    </nuxt-link>
 </template>
--- pages/guides/index.vue
+++ pages/guides/index.vue
@@ -2,5 +2,5 @@
 import basicCard from '~/components/image-card.vue';
 </script>
 <template>
-    <basicCard title="Build a Static Website!" image="https://kinsta.com/wp-content/uploads/2021/10/html-site-example.png" description="" link-url="/guides/"/> <!-- TODO: temp image, find a permanant -->
+    <basicCard title="Build a Static Website!" image="/assets/guides/basic-website/hero.png" description="learn how to make a basic website using html and css" link-url="/guides/basic-website"/> <!-- TODO: temp image, find a permanant -->
 </template>
