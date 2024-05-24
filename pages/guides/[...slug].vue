0d5692b265c4907cb10af6ef7539a1d607ede5eb
Dakota Roth <93889672+Codaea@users.noreply.github.com>
2024-05-24 01:26:02 +0000

    update slug for guides

--- pages/guides/[...slug].vue
+++ pages/guides/[...slug].vue
@@ -7,7 +7,8 @@
     <div class="flex justify-center bg-gray-200 mx-auto w-3/5 rounded-2xl">
         <div>
           <ContentQuery :path="$route.path" find="one" v-slot="{ data }">
-            <h1 class="text-3xl font-bold">{{ data.title }}</h1>
+            <img :src=data.image alt="image" class="w-full h-96 mt-8 object-cover rounded-t-2xl" />
+            <h1 class="text-3xl font-bold mt-8">{{ data.title }}</h1>
             <h2 class="text-m font-bold text-slate-500">Author: {{ data.author }}</h2>
             <br>
             <ContentRenderer :value="data" class="prose" />
