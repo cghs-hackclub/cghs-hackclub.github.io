73ff33769864efa7c3560b3db520adc27329a979
Dakota Roth <dakotajayroth@gmail.com>
2024-08-25 10:02:29 -0700

    updated homepage

--- pages/index.vue
+++ pages/index.vue
@@ -8,17 +8,20 @@
       -->
       <img class="pointer-events-none" src="~/public/assets/hack-cghs-logo-dark.svg" alt="Hack Club Logo" />
     </div>
-    <div
-      class="flex flex-row h-[40rem] items-center justify-center"
-    >
+    <div class="flex flex-col h-[20rem] items-center justify-center space-x-5 space-y-8">
+      <p class="ml-8 text-7xl" >&emsp; A Place For Creatives And Makers.</p>
+      <p class="text-3xl">Hack Club is a place for builders and makers to connect, and create together.
+        We host regular workshops, hackathons, and hands-on projects that empower members to turn their ideas into reality. By being a part of Hack Club, you'll gain access to a global network of like-minded hackers and makers, as well as a wealth of resources designed to support you in your journey
+      </p>
+    </div>
+    <div class="flex flex-row h-[40rem] items-center justify-center">
       <h1 class="ml-8 text-7xl">
         We Aren't "Hacking The Mainframe."
       </h1>
       <div>
         <p class=" text-3xl">
           Hack Club is a space for people who like to make and understand how
-          things work. we do <a class="underline text-blue-500" href="https://jams.hackclub.com">workshops and jams</a>, where we make our own
-          software and hardware.
+          things work. we don't hack the system, we build.
         </p>
 
         <p class="mt-5 text-m">
