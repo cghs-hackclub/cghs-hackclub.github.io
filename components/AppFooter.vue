5db1374b083c36f469668cefb3825d02b4275ea1
Dakota Roth <dakotajayroth@gmail.com>
2024-08-25 09:55:55 -0700

    updated footer

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,15 +1,32 @@
 <template>
-  <div>
-    <ul>
-      <li><a href="https://hackclub.com/conduct/">Code of Conduct</a></li>
-    </ul>
+  <div class="flex flex-row space-x-6 ml-64">
+    <div>
+      <p class="text-xl font-bold">Resources</p>
+      <ul>
+        <li>
+          <NuxtLink to="https://hackclub.com">Main Hackclub Site</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/projects">Projects</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/workshops">Workshops</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="https://hackclub.com/">Code Of Conduct</NuxtLink>
+        </li>
+      </ul>
+    </div>
+    <div>
+      <p class="text-xl font-bold">Other Places</p>
+      <ul>
+        <li>
+          <NuxtLink to="https://instagram.com/hackclub">Instagram</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="https://github.com/cghs-hackclub">Github</NuxtLink>
+        </li>
+        </ul>
+    </div>
   </div>
 </template>
-
-<script lang="ts" setup>
-
-</script>
-
-<style>
-
-</style>
